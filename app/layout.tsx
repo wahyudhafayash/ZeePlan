d3b8980778e60b306b6088eb6983d26e98ba0aaf
fayass <wahyudhafayash@gmail.com>
2025-09-16 20:32:39 +0700

    Setup db

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Provider>{children}</Provider>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
         </body>
       </html>
     </ClerkProvider>
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+"use client";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "./_components/Header";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
+import { UserDetailContext } from "@/context/UserDetailContext";
 
 const Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const CreateUser = useMutation(api.user.CreateNewUser);
+  const [userDetail, setUserDetail] = useState<any>(null);
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && CreateNewUser();
+  }, [user]);
+
+  const CreateNewUser = async () => {
+    if (user) {
+      // Save New User if not exists
+      const result = await CreateUser({
+        email: user?.primaryEmailAddress?.emailAddress ?? "",
+        imageUrl: user?.imageUrl,
+        name: user?.fullName ?? "",
+      });
+      setUserDetail(result);
+    }
+  };
+
   return (
-    <div>
-      <Header />
-      {children}
-    </div>
+    <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+      <div>
+        <Header />
+        {children}
+      </div>
+    </UserDetailContext.Provider>
   );
 };
 
 export default Provider;
+
+export const useUserDetail = () => {
+  return useContext(UserDetailContext);
+};
